c3c778448b00ddd3d7b4a81a6f790db5e07ca67c
Baoan Nguyen <baoansayz@gmail.com>
2017-04-23 17:06:07 -0500

    Progress on chatReq and sendMSG

--- src/client_code/Client.java
+++ src/client_code/Client.java
@@ -2,6 +2,7 @@ package client_code;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.Scanner;
 import java.math.BigInteger;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
@@ -32,12 +33,16 @@ public class Client{
 	private String clientID;
 	private int secretkey = 123456;
 	private BouncyEncryption encryptor = null;
+	private boolean connected;
+	private boolean serverConnect;
+	private Scanner scanner;
 
 	Client ()throws SocketException, UnknownHostException{
 		buffer = new byte[1024];
 		datagramSocket = new DatagramSocket();
 		address = InetAddress.getLocalHost();
 		packet = new DatagramPacket(buffer, buffer.length, address, 8888);
+		scanner = new Scanner (System.in);
 	}
 
 	public void sendLogin()throws IOException, NoSuchAlgorithmException, ShortBufferException, IllegalBlockSizeException, BadPaddingException, InvalidKeyException, NoSuchProviderException, NoSuchPaddingException, InvalidAlgorithmParameterException{
@@ -51,7 +56,8 @@ public class Client{
 		// receive auth
 		datagramSocket.receive(packet);
 		String s = unpack(packet);
-    
+		// check to see if user exists on the server
+    	if (!s.contains("User")){
     	int rand = Integer.parseInt(s);
     	int key = rand + secretkey;
 
@@ -85,18 +91,76 @@ public class Client{
     	datagramSocket.receive(packet);
     
     	String strdecrypt = encryptor.Decrypt(packet.getData());
-    	
+   
     	System.out.println(strdecrypt);
+    	serverConnect = true;
+    }
+    else{
+    	System.out.println("User DNE");
+    	// exit
+    	System.exit(0);
+    }
+
 	}
                  
-	public void chatRequest(String client){
+	public void chatRequest()throws IOException{
+		String second_client;
 		// request connection with 2nd chat client
+		 do {
+		 	second_client = scanner.nextLine();
+		 	// use bouncy encryption???
 
-		System.out.println ("Chat Started with " + client);
+
+		 	DatagramPacket packet = Packet_Helpers.stringToPacket(second_client, address, 4445);
+		 	datagramSocket.send(packet);
+		 	// receive something from server 
+
+		 	//if 2nd client is avaliable
+		 		// connected = true
+
+		 	// else "User DNE, try again"
+		 }while (!connected);
+
+
+
+		
+
+		System.out.println ("Chat Started with " + second_client);
+		
 	}
 
-	public void sendMSG(String msg){
-		// send message to
+	public void sendMSG() throws ShortBufferException, IllegalBlockSizeException, BadPaddingException,
+	IOException{
+		// send message to server
+		String msg;
+		 do {
+			System.out.print("[You]:    ");
+			msg = scanner.nextLine();
+			System.out.println (msg);
+			if (msg.equals("End Chat") == true)
+				 break;
+			
+			else
+				{
+					// else send msg to server
+					byte [] encrypteddata = null;
+					encrypteddata = encryptor.Encrypt(msg);
+					packet = Packet_Helpers.arrayToPacket(encrypteddata, address, 4445);
+
+					try {
+						datagramSocket.send(packet);
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+
+				}
+			}
+			 while (true);
+
+		// close TCP connections
+		//endChat();
+		System.out.println("Chat ended");
 	}
 
 	public void rcvMSG(DatagramPacket msg) throws IOException{
@@ -137,7 +201,6 @@ public class Client{
 public static void main(String[] args) throws UnknownHostException, SocketException, IOException, NoSuchAlgorithmException, ShortBufferException, IllegalBlockSizeException, BadPaddingException, InvalidKeyException, NoSuchProviderException, NoSuchPaddingException, InvalidAlgorithmParameterException  {
 
 	Client a = new Client();
-
 	a.sendLogin();
 
 }
